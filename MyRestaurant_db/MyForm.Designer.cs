ee6a3bce83877c71a518c0f414e1c07f029cd9ff
Pico4ogo <31734893+Pico4ogo@users.noreply.github.com>
2018-06-25 18:40:08 +0300

    Add project files.

---
+++ MyRestaurant_db/MyForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace MyRestaurant_db
+{
+    partial class MyForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Number = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gridName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Number,
+            this.gridName,
+            this.Column1});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(252, 266);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Number
+            // 
+            this.Number.HeaderText = "#";
+            this.Number.Name = "Number";
+            this.Number.Width = 35;
+            // 
+            // gridName
+            // 
+            this.gridName.HeaderText = "Название блюда";
+            this.gridName.Name = "gridName";
+            this.gridName.Width = 120;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Цена";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 50;
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(64, 284);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(75, 23);
+            this.button.TabIndex = 2;
+            this.button.Text = "Show";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 284);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(46, 20);
+            this.textBox.TabIndex = 3;
+            this.textBox.Text = "1";
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView2.Location = new System.Drawing.Point(270, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(450, 266);
+            this.dataGridView2.TabIndex = 4;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Название";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Рецепт";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 200;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Продукты";
+            this.Column4.Name = "Column4";
+            // 
+            // MyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 335);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "MyForm";
+            this.Text = "RestaurantDB";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Number;
+        private System.Windows.Forms.DataGridViewTextBoxColumn gridName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
+
---
+++ MyRestaurant_db/MyForm.cs
@@ -0,0 +1,125 @@
+﻿using MyRestaurant_db.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyRestaurant_db
+{
+    public partial class MyForm : Form
+    {
+        public MyForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var customers = GetDishesEf();
+            int i = 0;
+            dataGridView1.RowCount = 10;
+            dataGridView1.ColumnCount = 3;
+            foreach (var customer in customers)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = customer.id_dish;
+                dataGridView1.Rows[i].Cells[1].Value = customer.dish_name;
+                dataGridView1.Rows[i].Cells[2].Value = customer.dish_price;
+                i += 1;
+            }
+        }
+
+        private static List<dish> GetDishesEf()
+        {
+            var context = new restaurant_dbContext();
+            IQueryable<dish> query = context.dish;
+            List<dish> customers = query.ToList();
+            return customers;
+        }
+        private static List<dish> GetDishesEf(int n)
+        {
+            var context = new restaurant_dbContext();
+            IQueryable<dish> query = context.dish.Where(c => c.id_dish == n);
+            List<dish> customers = query.ToList();
+            return customers;
+        }
+        private static List<recipes> GetRecipesEf(int n)
+        {
+            var context = new restaurant_dbContext();
+            IQueryable<recipes> query = context.recipes.Where(c => c.dish_id == n);
+            List<recipes> customers = query.ToList();
+            return customers;
+        }
+        private static List<ingredients> GetIngredientsEf(int n)
+        {
+            var context = new restaurant_dbContext();
+            IQueryable<ingredients> query = context.ingredients.Where(c => c.id_ingredient == n);
+            List<ingredients> customers = query.ToList();
+            return customers;
+        }
+
+        private static List<MyProxy> GetCustomers()
+        {
+            using (IDbConnection connection = new SqlConnection(Settings.Default.dbConnect))
+            {
+                IDbCommand command = new SqlCommand("SELECT * FROM dish");
+                command.Connection = connection;
+
+                connection.Open();
+
+                IDataReader reader = command.ExecuteReader();
+
+                List<MyProxy> customers = new List<MyProxy>();
+
+                while (reader.Read())
+                {
+                    MyProxy customer = new MyProxy();
+                    customer.Id = reader.GetInt32(0);
+                    customer.Name = reader.GetString(1);
+                    customers.Add(customer);
+                }
+                return customers;
+            }
+        }
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void button_Click(object sender, EventArgs e)
+        {
+            int numRec = 1;
+            try
+            {
+                numRec = Convert.ToInt32(textBox.Text);
+            } catch (Exception)
+            {
+                MessageBox.Show("Введите номер блюда.");
+            }
+            req(numRec);
+        }
+        private void req(int numRec)
+        {
+            dataGridView2.RowCount = 1;
+            dataGridView2.ColumnCount = 3;
+            var dishes = GetDishesEf(numRec);
+            var recipes = GetRecipesEf(numRec);
+            var ingredients = GetIngredientsEf(numRec);
+            foreach (var dish_tmp in dishes)
+            {
+                dataGridView2.Rows[0].Cells[0].Value = dish_tmp.dish_name;
+            }
+            foreach (var recipe_tmp in recipes)
+            {
+                dataGridView2.Rows[0].Cells[1].Value = recipe_tmp.recipe;
+            }
+            foreach (var ingredient_tmp in ingredients)
+            {
+                dataGridView2.Rows[0].Cells[2].Value = ingredient_tmp.ingredients1;
+            }
+        }
+    }
+}
